37cd8a5483857ee069f6ee0b5517227dc98c818e
Basel85 <80516466+Basel85@users.noreply.github.com>
2022-07-30 15:04:54 +0200

    Tasks

--- Task 5/eCommerce/App/Database/Models/Contracts/Crud.php
+++
@@ -1,9 +0,0 @@
-<?php
-  namespace App\Database\Models\Contracts;
-  interface Crud{
-    function create();
-    function read();
-    function update();
-    function delete();
-  }
-?>
--- Task 5/eCommerce/App/Http/Middlewares/Auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-// check if user guest
-if(empty($_SESSION['user'])){
-    // prevent guest user to show login page
-    header('location:login.php');die;
-}
